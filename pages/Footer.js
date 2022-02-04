38f4e01726ac8cb5463fe7abe9bf436345bda3fd
Suren Partheepan <ravinthiranpartheepan1407@gmail.com>
2022-02-04 04:12:25 -0800

    Circle CI

--- pages/Footer.js
+++ pages/Footer.js
@@ -2,21 +2,21 @@ import React from 'react';
 
 function Footer(){
   return(
-    <footer class="bg-pink-900 p-8 text-center text-white">
-    <div class="container p-6">
-      <div class="">
-        <p class="flex justify-center items-center">
-          <span class="mr-4">Join My Test DAO App</span>
-          <a type="button" class="inline-block px-6 py-2 border-2 border-white text-white font-medium text-xs leading-tight uppercase rounded-full hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out" href="https://minter-app-ravinthiranpartheepan1407.vercel.app/">
+    <footer className="bg-pink-900 p-8 text-center text-white">
+    <div className="container p-6">
+      <div>
+        <p className="flex justify-center items-center">
+          <span className="mr-4">Join My Test DAO App</span>
+          <a type="button" className="inline-block px-6 py-2 border-2 border-white text-white font-medium text-xs leading-tight uppercase rounded-full hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out" href="https://minter-app-ravinthiranpartheepan1407.vercel.app/">
             DAO
             </a>
         </p>
       </div>
     </div>
 
-    <div class="text-center p-4">
+    <div className="text-center p-4">
       © 2021 Copyright:
-      <a class="text-white" href="/">Developed By Ravinthiran Partheepan</a>
+      <a className="text-white" href="/">Developed By Ravinthiran Partheepan</a>
     </div>
     </footer>
   )
--- pages/Header.js
+++ pages/Header.js
@@ -2,18 +2,18 @@ import React from 'react';
 
 function Header(){
   return(
-    <ul class="flex bg-pink-900 p-8">
-  <li class="flex-1 mr-2">
-    <a class="text-center block rounded hover:border-gray-200 text-white hover:bg-gray-200 py-2 px-4" href="https://eth-gas-tracker-xi.vercel.app/">Gas Tracker</a>
+    <ul className="flex bg-pink-900 p-8">
+  <li className="flex-1 mr-2">
+    <a className="text-center block rounded hover:border-gray-200 text-white hover:bg-gray-200 py-2 px-4" href="https://eth-gas-tracker-xi.vercel.app/">Gas Tracker</a>
   </li>
-  <li class="flex-1 mr-2">
-    <a class="text-center block rounded hover:border-gray-200 text-white hover:bg-gray-200 py-2 px-4" href="https://minter-app-ravinthiranpartheepan1407.vercel.app/">DAO</a>
+  <li className="flex-1 mr-2">
+    <a className="text-center block rounded hover:border-gray-200 text-white hover:bg-gray-200 py-2 px-4" href="https://minter-app-ravinthiranpartheepan1407.vercel.app/">DAO</a>
   </li>
-  <li class="text-center flex-1">
-    <a class="text-center block rounded hover:border-gray-200 text-white hover:bg-gray-200 py-2 px-4" href="#">DeFi</a>
+  <li className="text-center flex-1">
+    <a className="text-center block rounded hover:border-gray-200 text-white hover:bg-gray-200 py-2 px-4" href="#">DeFi</a>
   </li>
-  <li class="text-center flex-1">
-    <a class="text-center block rounded hover:border-gray-200 text-blue-500 hover:bg-gray-200 py-2 px-4" href="/">Home</a>
+  <li className="text-center flex-1">
+    <a className="text-center block rounded hover:border-gray-200 text-blue-500 hover:bg-gray-200 py-2 px-4" href="/">Home</a>
   </li>
 
 </ul>
